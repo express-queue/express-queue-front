b643574b065b0827703e58b6005c3382f69f821f
Ayrat <gimranov45@gmail.com>
2022-04-28 02:03:54 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <p className='draggable' draggable='true'>1</p>
+        <p className='draggable' draggable='true'>2</p>        
+      </div>
+      <div className='container'>
+        <p className='draggable' draggable='true'>3</p>
+        <p className='draggable' draggable='true'>4</p>        
+      </div>
+    </>
+  );
+}
+
+export default App;
